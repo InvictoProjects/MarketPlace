4e434e8c8ebe3735c3fdff96106888acfe142dc2
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-02-06 19:07:25 +0200

    Init commit

---
+++ src/main/kotlin/com/invictoprojects/marketplace/MarketPlaceApplication.kt
@@ -0,0 +1,11 @@
+package com.invictoprojects.marketplace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MarketPlaceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MarketPlaceApplication>(*args)
+}
