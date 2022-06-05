7bcef88ec710e3ab6f10cf03b86cd02771e5fb1e
Vladyslav <proxima2002vt@gmail.com>
2022-06-05 19:31:54 +0300

    [MP-3] Update user model

--- src/main/kotlin/com/invictoprojects/marketplace/persistence/model/User.kt
+++ src/main/kotlin/com/invictoprojects/marketplace/persistence/model/User.kt
@@ -1,12 +1,12 @@
 package com.invictoprojects.marketplace.persistence.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "users")
 class User(
+    var email: String,
+    @Column(name = "password_hash") var passwordHash: String,
+    @Column(name="role_type") var role: Role = Role.USER,
     @Id @GeneratedValue var id: Long? = null
 )
