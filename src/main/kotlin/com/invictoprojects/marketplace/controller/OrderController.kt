72c325b37a7c51fdeeded7475454b9b5740026d0
Vladyslav <proxima2002vt@gmail.com>
2022-06-16 20:08:37 +0300

    [MP-6] Update order controller

--- src/main/kotlin/com/invictoprojects/marketplace/controller/OrderController.kt
+++ src/main/kotlin/com/invictoprojects/marketplace/controller/OrderController.kt
@@ -2,12 +2,14 @@ package com.invictoprojects.marketplace.controller
 
 import com.invictoprojects.marketplace.dto.MappingUtils
 import com.invictoprojects.marketplace.dto.OrderCreationDto
+import com.invictoprojects.marketplace.dto.OrderDetailDto
 import com.invictoprojects.marketplace.dto.OrderDto
 import com.invictoprojects.marketplace.service.OrderService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api/orders")
@@ -30,6 +32,22 @@ class OrderController (private val orderService: OrderService) {
     @ResponseBody
     fun getAllOrders(): ResponseEntity<List<OrderDto>> {
         val orders = orderService.findAll().map { order -> MappingUtils.convertToDto(order) }.toList()
+
+        return ResponseEntity.ok().body(orders)
+    }
+
+    @GetMapping("/{id}/details")
+    @ResponseBody
+    fun getOrderProducts(@PathVariable id: Long): ResponseEntity<List<OrderDetailDto>> {
+        val details = orderService.findOrderProductsByOrder(id).map { order -> MappingUtils.convertToDto(order) }.toList()
+
+        return ResponseEntity.ok().body(details)
+    }
+
+    @GetMapping("/{id}/period")
+    @ResponseBody
+    fun getOrderByPeriod(@PathVariable id: Long, @RequestBody start: Date, @RequestBody end: Date): ResponseEntity<List<OrderDto>> {
+        val orders = orderService.findByDate(start, end).map { order -> MappingUtils.convertToDto(order) }.toList()
 
         return ResponseEntity.ok().body(orders)
     }
@@ -61,6 +79,34 @@ class OrderController (private val orderService: OrderService) {
             val order = MappingUtils.convertToEntity(orderCreationDto)
             order.id = id
             val updatedOrder = orderService.update(order)
+            val result = MappingUtils.convertToDto(updatedOrder)
+
+            ResponseEntity.ok().body(result)
+        } catch (e: IllegalArgumentException) {
+            ResponseEntity(mapOf("error" to e.message), HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @PutMapping("/{id}/products/add/{productId}/{amount}")
+    @ResponseBody
+    fun addOrderProducts(@PathVariable id: Long, @PathVariable productId: Long, @PathVariable amount: Int): ResponseEntity<Any> {
+        return try {
+            orderService.addProduct(id, productId, amount)
+            val updatedOrder = orderService.findById(id)
+            val result = MappingUtils.convertToDto(updatedOrder)
+
+            ResponseEntity.ok().body(result)
+        } catch (e: IllegalArgumentException) {
+            ResponseEntity(mapOf("error" to e.message), HttpStatus.BAD_REQUEST)
+        }
+    }
+
+    @PutMapping("/{id}/products/remove/{productId}/{amount}")
+    @ResponseBody
+    fun removeOrderProducts(@PathVariable id: Long, @PathVariable productId: Long, @PathVariable amount: Int): ResponseEntity<Any> {
+        return try {
+            orderService.removeProduct(id, productId, amount)
+            val updatedOrder = orderService.findById(id)
             val result = MappingUtils.convertToDto(updatedOrder)
 
             ResponseEntity.ok().body(result)
