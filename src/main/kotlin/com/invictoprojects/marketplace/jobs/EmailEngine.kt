8cf833cde970b4d285e56f7b10e23aed379c8a98
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-06-18 21:05:08 +0300

    [MP-14] Update recommendation email to send three random products

--- src/main/kotlin/com/invictoprojects/marketplace/jobs/EmailEngine.kt
+++ src/main/kotlin/com/invictoprojects/marketplace/jobs/EmailEngine.kt
@@ -1,6 +1,9 @@
 package com.invictoprojects.marketplace.jobs
 
+import com.invictoprojects.marketplace.persistence.model.Product
 import com.invictoprojects.marketplace.service.EmailService
+import com.invictoprojects.marketplace.service.ProductService
+import com.invictoprojects.marketplace.service.UserService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
@@ -8,13 +11,29 @@ import org.springframework.stereotype.Component
 @Component
 class EmailEngine(
     private val emailService: EmailService,
+    private val userService: UserService,
+    private val productService: ProductService,
     @Value("\${marketplace.email.recommendation.sender}") private val sender: String,
     @Value("\${marketplace.email.recommendation.personal}") private val personal: String
 ) {
 
-    @Scheduled(cron = "* * * * * *")
+    @Scheduled(cron = "*/20 * * * * *")
     fun sendProductRecommendationEmails() {
-        emailService.sendEmail(sender, personal, sender, subject, text)
+        val products = productService.findAll()
+            .toList()
+
+        userService.findAll()
+            .forEach { user ->
+                val recommendedProducts = mutableListOf<Product>()
+                for (i in 0..2) {
+                    val randomIndex = (products.indices).random()
+                    val randomProduct = products[randomIndex]
+                    recommendedProducts.add(randomProduct)
+                }
+
+                val emailText = createRecommendationEmailText(recommendedProducts)
+                emailService.sendEmail(sender, personal, user.email, subject, emailText)
+            }
     }
 
     companion object {
@@ -29,9 +48,18 @@ class EmailEngine(
                 "</head>\n" +
                 "<body>\n" +
                 "<p>Hi there,</p>\n" +
-                "<p>This is test email</p>\n" +
+                "<p>This is random 5 products recommended for you: %s</p>\n" +
                 "</body>\n" +
                 "</html>"
+
+        private fun createRecommendationEmailText(products: List<Product>): String {
+            val productNames = StringBuilder()
+            for (product in products) {
+                productNames.append(product.name)
+                    .append(", ")
+            }
+            return String.format(text, productNames.trim())
+        }
     }
 
 }
