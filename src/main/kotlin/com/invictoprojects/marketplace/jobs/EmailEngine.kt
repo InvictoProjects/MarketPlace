05b0f573f8339dff214163c58a2967b7724fb2a0
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-06-19 13:17:00 +0300

    [MP-14] Move email templates from code

--- src/main/kotlin/com/invictoprojects/marketplace/jobs/EmailEngine.kt
+++ src/main/kotlin/com/invictoprojects/marketplace/jobs/EmailEngine.kt
@@ -4,17 +4,21 @@ import com.invictoprojects.marketplace.persistence.model.Product
 import com.invictoprojects.marketplace.service.EmailService
 import com.invictoprojects.marketplace.service.ProductService
 import com.invictoprojects.marketplace.service.UserService
+import freemarker.template.Configuration
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.io.StringWriter
 
 @Component
 class EmailEngine(
     private val emailService: EmailService,
     private val userService: UserService,
     private val productService: ProductService,
+    private val configuration: Configuration,
     @Value("\${marketplace.email.recommendation.sender}") private val sender: String,
-    @Value("\${marketplace.email.recommendation.personal}") private val personal: String
+    @Value("\${marketplace.email.recommendation.personal}") private val personal: String,
+    @Value("marketplace.email.recommendation.subject") private val subject: String
 ) {
 
     @Scheduled(cron = "*/20 * * * * *")
@@ -32,369 +36,28 @@ class EmailEngine(
                     recommendedProducts.add(randomProduct)
                 }
 
-                val emailText = createRecommendationEmailText(recommendedProducts)
+                val productText = createProductText(recommendedProducts)
+                val stringWriter = StringWriter()
+                configuration.getTemplate("recommendation_email.ftlh").process(
+                    mapOf("products" to productText),
+                    stringWriter
+                )
+
+                val emailText = stringWriter.buffer.toString()
                 emailService.sendEmail(sender, personal, user.email, subject, emailText)
             }
     }
 
-    companion object {
-
-        private const val subject = "MarketPlace - products that are recommended especially for you"
-        private const val text = "<!doctype html>\n" +
-                "<html>\n" +
-                "  <head>\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n" +
-                "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n" +
-                "    <title>Simple Transactional Email</title>\n" +
-                "    <style>\n" +
-                "      img {\n" +
-                "        border: none;\n" +
-                "        -ms-interpolation-mode: bicubic;\n" +
-                "        max-width: 100%%; \n" +
-                "      }\n" +
-                "\n" +
-                "      body {\n" +
-                "        background-color: #f6f6f6;\n" +
-                "        font-family: sans-serif;\n" +
-                "        -webkit-font-smoothing: antialiased;\n" +
-                "        font-size: 14px;\n" +
-                "        line-height: 1.4;\n" +
-                "        margin: 0;\n" +
-                "        padding: 0;\n" +
-                "        -ms-text-size-adjust: 100%%;\n" +
-                "        -webkit-text-size-adjust: 100%%; \n" +
-                "      }\n" +
-                "\n" +
-                "      table {\n" +
-                "        border-collapse: separate;\n" +
-                "        mso-table-lspace: 0pt;\n" +
-                "        mso-table-rspace: 0pt;\n" +
-                "        width: 100%%; }\n" +
-                "        table td {\n" +
-                "          font-family: sans-serif;\n" +
-                "          font-size: 14px;\n" +
-                "          vertical-align: top; \n" +
-                "      }\n" +
-                "      \n" +
-                "      .body {\n" +
-                "        background-color: #f6f6f6;\n" +
-                "        width: 100%%; \n" +
-                "      }\n" +
-                "      \n" +
-                "      .container {\n" +
-                "        display: block;\n" +
-                "        margin: 0 auto !important;\n" +
-                "        max-width: 580px;\n" +
-                "        padding: 10px;\n" +
-                "        width: 580px; \n" +
-                "      }\n" +
-                "      \n" +
-                "      .content {\n" +
-                "        box-sizing: border-box;\n" +
-                "        display: block;\n" +
-                "        margin: 0 auto;\n" +
-                "        max-width: 580px;\n" +
-                "        padding: 10px; \n" +
-                "      }\n" +
-                "      \n" +
-                "      .main {\n" +
-                "        background: #ffffff;\n" +
-                "        border-radius: 3px;\n" +
-                "        width: 100%%; \n" +
-                "      }\n" +
-                "\n" +
-                "      .wrapper {\n" +
-                "        box-sizing: border-box;\n" +
-                "        padding: 20px; \n" +
-                "      }\n" +
-                "\n" +
-                "      .content-block {\n" +
-                "        padding-bottom: 10px;\n" +
-                "        padding-top: 10px;\n" +
-                "      }\n" +
-                "\n" +
-                "      .footer {\n" +
-                "        clear: both;\n" +
-                "        margin-top: 10px;\n" +
-                "        text-align: center;\n" +
-                "        width: 100%%; \n" +
-                "      }\n" +
-                "        .footer td,\n" +
-                "        .footer p,\n" +
-                "        .footer span,\n" +
-                "        .footer a {\n" +
-                "          color: #999999;\n" +
-                "          font-size: 12px;\n" +
-                "          text-align: center; \n" +
-                "      }\n" +
-                "\n" +
-                "      h1,\n" +
-                "      h2,\n" +
-                "      h3,\n" +
-                "      h4 {\n" +
-                "        color: #000000;\n" +
-                "        font-family: sans-serif;\n" +
-                "        font-weight: 400;\n" +
-                "        line-height: 1.4;\n" +
-                "        margin: 0;\n" +
-                "        margin-bottom: 30px; \n" +
-                "      }\n" +
-                "\n" +
-                "      h1 {\n" +
-                "        font-size: 35px;\n" +
-                "        font-weight: 300;\n" +
-                "        text-align: center;\n" +
-                "        text-transform: capitalize; \n" +
-                "      }\n" +
-                "\n" +
-                "      p,\n" +
-                "      ul,\n" +
-                "      ol {\n" +
-                "        font-family: sans-serif;\n" +
-                "        font-size: 14px;\n" +
-                "        font-weight: normal;\n" +
-                "        margin: 0;\n" +
-                "        margin-bottom: 15px; \n" +
-                "      }\n" +
-                "        p li,\n" +
-                "        ul li,\n" +
-                "        ol li {\n" +
-                "          list-style-position: inside;\n" +
-                "          margin-left: 5px; \n" +
-                "      }\n" +
-                "\n" +
-                "      a {\n" +
-                "        color: #3498db;\n" +
-                "        text-decoration: underline; \n" +
-                "      }\n" +
-                "\n" +
-                "      .btn {\n" +
-                "        box-sizing: border-box;\n" +
-                "        width: 100%%; }\n" +
-                "        .btn > tbody > tr > td {\n" +
-                "          padding-bottom: 15px; }\n" +
-                "        .btn table {\n" +
-                "          width: auto; \n" +
-                "      }\n" +
-                "        .btn table td {\n" +
-                "          background-color: #ffffff;\n" +
-                "          border-radius: 5px;\n" +
-                "          text-align: center; \n" +
-                "      }\n" +
-                "        .btn a {\n" +
-                "          background-color: #ffffff;\n" +
-                "          border: solid 1px #3498db;\n" +
-                "          border-radius: 5px;\n" +
-                "          box-sizing: border-box;\n" +
-                "          color: #3498db;\n" +
-                "          cursor: pointer;\n" +
-                "          display: inline-block;\n" +
-                "          font-size: 14px;\n" +
-                "          font-weight: bold;\n" +
-                "          margin: 0;\n" +
-                "          padding: 12px 25px;\n" +
-                "          text-decoration: none;\n" +
-                "          text-transform: capitalize; \n" +
-                "      }\n" +
-                "\n" +
-                "      .btn-primary table td {\n" +
-                "        background-color: #3498db; \n" +
-                "      }\n" +
-                "\n" +
-                "      .btn-primary a {\n" +
-                "        background-color: #3498db;\n" +
-                "        border-color: #3498db;\n" +
-                "        color: #ffffff; \n" +
-                "      }\n" +
-                "\n" +
-                "      .last {\n" +
-                "        margin-bottom: 0; \n" +
-                "      }\n" +
-                "\n" +
-                "      .first {\n" +
-                "        margin-top: 0; \n" +
-                "      }\n" +
-                "\n" +
-                "      .align-center {\n" +
-                "        text-align: center; \n" +
-                "      }\n" +
-                "\n" +
-                "      .align-right {\n" +
-                "        text-align: right; \n" +
-                "      }\n" +
-                "\n" +
-                "      .align-left {\n" +
-                "        text-align: left; \n" +
-                "      }\n" +
-                "\n" +
-                "      .clear {\n" +
-                "        clear: both; \n" +
-                "      }\n" +
-                "\n" +
-                "      .mt0 {\n" +
-                "        margin-top: 0; \n" +
-                "      }\n" +
-                "\n" +
-                "      .mb0 {\n" +
-                "        margin-bottom: 0; \n" +
-                "      }\n" +
-                "\n" +
-                "      .preheader {\n" +
-                "        color: transparent;\n" +
-                "        display: none;\n" +
-                "        height: 0;\n" +
-                "        max-height: 0;\n" +
-                "        max-width: 0;\n" +
-                "        opacity: 0;\n" +
-                "        overflow: hidden;\n" +
-                "        mso-hide: all;\n" +
-                "        visibility: hidden;\n" +
-                "        width: 0; \n" +
-                "      }\n" +
-                "\n" +
-                "      .powered-by a {\n" +
-                "        text-decoration: none; \n" +
-                "      }\n" +
-                "\n" +
-                "      hr {\n" +
-                "        border: 0;\n" +
-                "        border-bottom: 1px solid #f6f6f6;\n" +
-                "        margin: 20px 0; \n" +
-                "      }\n" +
-                "\n" +
-                "      @media only screen and (max-width: 620px) {\n" +
-                "        table.body h1 {\n" +
-                "          font-size: 28px !important;\n" +
-                "          margin-bottom: 10px !important; \n" +
-                "        }\n" +
-                "        table.body p,\n" +
-                "        table.body ul,\n" +
-                "        table.body ol,\n" +
-                "        table.body td,\n" +
-                "        table.body span,\n" +
-                "        table.body a {\n" +
-                "          font-size: 16px !important; \n" +
-                "        }\n" +
-                "        table.body .wrapper,\n" +
-                "        table.body .article {\n" +
-                "          padding: 10px !important; \n" +
-                "        }\n" +
-                "        table.body .content {\n" +
-                "          padding: 0 !important; \n" +
-                "        }\n" +
-                "        table.body .container {\n" +
-                "          padding: 0 !important;\n" +
-                "          width: 100%% !important; \n" +
-                "        }\n" +
-                "        table.body .main {\n" +
-                "          border-left-width: 0 !important;\n" +
-                "          border-radius: 0 !important;\n" +
-                "          border-right-width: 0 !important; \n" +
-                "        }\n" +
-                "        table.body .btn table {\n" +
-                "          width: 100%% !important; \n" +
-                "        }\n" +
-                "        table.body .btn a {\n" +
-                "          width: 100%% !important; \n" +
-                "        }\n" +
-                "        table.body .img-responsive {\n" +
-                "          height: auto !important;\n" +
-                "          max-width: 100%% !important;\n" +
-                "          width: auto !important; \n" +
-                "        }\n" +
-                "      }\n" +
-                "\n" +
-                "      @media all {\n" +
-                "        .ExternalClass {\n" +
-                "          width: 100%%; \n" +
-                "        }\n" +
-                "        .ExternalClass,\n" +
-                "        .ExternalClass p,\n" +
-                "        .ExternalClass span,\n" +
-                "        .ExternalClass font,\n" +
-                "        .ExternalClass td,\n" +
-                "        .ExternalClass div {\n" +
-                "          line-height: 100%%; \n" +
-                "        }\n" +
-                "        .apple-link a {\n" +
-                "          color: inherit !important;\n" +
-                "          font-family: inherit !important;\n" +
-                "          font-size: inherit !important;\n" +
-                "          font-weight: inherit !important;\n" +
-                "          line-height: inherit !important;\n" +
-                "          text-decoration: none !important; \n" +
-                "        }\n" +
-                "        #MessageViewBody a {\n" +
-                "          color: inherit;\n" +
-                "          text-decoration: none;\n" +
-                "          font-size: inherit;\n" +
-                "          font-family: inherit;\n" +
-                "          font-weight: inherit;\n" +
-                "          line-height: inherit;\n" +
-                "        }\n" +
-                "        .btn-primary table td:hover {\n" +
-                "          background-color: #34495e !important; \n" +
-                "        }\n" +
-                "        .btn-primary a:hover {\n" +
-                "          background-color: #34495e !important;\n" +
-                "          border-color: #34495e !important; \n" +
-                "        } \n" +
-                "      }\n" +
-                "\n" +
-                "    </style>\n" +
-                "  </head>\n" +
-                "  <body>\n" +
-                "    <span class=\"preheader\">MarketPlace - products that are recommended especially for you</span>\n" +
-                "    <table role=\"presentation\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" class=\"body\">\n" +
-                "      <tr>\n" +
-                "        <td>&nbsp;</td>\n" +
-                "        <td class=\"container\">\n" +
-                "          <div class=\"content\">\n" +
-                "\n" +
-                "            <table role=\"presentation\" class=\"main\">\n" +
-                "            \n" +
-                "              <tr>\n" +
-                "                <td class=\"wrapper\">\n" +
-                "                  <table role=\"presentation\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">\n" +
-                "                    <tr>\n" +
-                "                      <td>\n" +
-                "                        <p>Hi there,</p>\n" +
-                "                        <p>You may also like these products: %s</p>\n" +
-                "                        <p>Have a nice day!</p>\n" +
-                "                      </td>\n" +
-                "                    </tr>\n" +
-                "                  </table>\n" +
-                "                </td>\n" +
-                "              </tr>\n" +
-                "\n" +
-                "            </table>\n" +
-                "\n" +
-                "            <div class=\"footer\">\n" +
-                "              <table role=\"presentation\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">\n" +
-                "                <tr>\n" +
-                "                  <td class=\"content-block\">\n" +
-                "                    <span class=\"apple-link\">MarketPlace - you can find anything you want</span>\n" +
-                "                  </td>\n" +
-                "                </tr>\n" +
-                "              </table>\n" +
-                "            </div>\n" +
-                "\n" +
-                "          </div>\n" +
-                "        </td>\n" +
-                "        <td>&nbsp;</td>\n" +
-                "      </tr>\n" +
-                "    </table>\n" +
-                "  </body>\n" +
-                "</html>"
-
-        private fun createRecommendationEmailText(products: List<Product>): String {
-            val productNames = StringBuilder()
-            for (product in products) {
-                productNames.append(product.name)
-                    .append(", ")
+    private fun createProductText(products: List<Product>): String {
+        val productNames = StringBuilder()
+        for (i in products.indices) {
+            val product = products[i]
+            productNames.append(product.name)
+            if (i != products.size - 1) {
+                productNames.append(", ")
             }
-            return String.format(text, productNames.trim())
         }
+        return productNames.trim().toString()
     }
 
 }
