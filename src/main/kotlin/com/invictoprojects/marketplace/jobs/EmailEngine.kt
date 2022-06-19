c8f347118afb0d3ade47a9bc1435eee0eef19a8d
Koroliuk <koroliuk.yaroslav@gmail.com>
2022-06-19 18:35:29 +0300

    [MP-14] Update emailEngine

--- src/main/kotlin/com/invictoprojects/marketplace/jobs/EmailEngine.kt
+++ src/main/kotlin/com/invictoprojects/marketplace/jobs/EmailEngine.kt
@@ -2,20 +2,19 @@ package com.invictoprojects.marketplace.jobs
 
 import com.invictoprojects.marketplace.persistence.model.Product
 import com.invictoprojects.marketplace.service.EmailService
-import com.invictoprojects.marketplace.service.ProductService
-import com.invictoprojects.marketplace.service.UserService
+import com.invictoprojects.marketplace.service.RecommendationService
 import freemarker.template.Configuration
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.io.StringWriter
-import kotlinx.coroutines.*
 
 @Component
 class EmailEngine(
     private val emailService: EmailService,
-    private val userService: UserService,
-    private val productService: ProductService,
+    private val recommendationService: RecommendationService,
     private val configuration: Configuration,
     @Value("\${marketplace.email.recommendation.sender}") private val sender: String,
     @Value("\${marketplace.email.recommendation.personal}") private val personal: String,
@@ -24,20 +23,11 @@ class EmailEngine(
 
     @Scheduled(cron = "*/30 * * * * *")
     fun sendProductRecommendationEmails() = runBlocking {
-        val products = productService.findAll()
-            .toList()
-
-        userService.findAll()
-            .filter { user -> user.isSubscribed }
-            .forEach { user ->
+        recommendationService.getAllUsersWithRecommendedProducts()
+            .forEach { pair ->
                 launch {
-                    val recommendedProducts = mutableListOf<Product>()
-                    for (i in 0..2) {
-                        val randomIndex = (products.indices).random()
-                        val randomProduct = products[randomIndex]
-                        recommendedProducts.add(randomProduct)
-                    }
-
+                    val user = pair.first
+                    val recommendedProducts = pair.second
                     val productText = createProductText(recommendedProducts)
                     val stringWriter = StringWriter()
                     configuration.getTemplate("recommendation_email.ftlh").process(
