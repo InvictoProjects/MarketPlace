54c76044b2bee2f53b733d15dd6ee1e68246c755
Vlad <vladholik84@gmail.com>
2022-06-21 20:30:02 +0300

    Adding sign up test

--- src/test/kotlin/com/invictoprojects/marketplace/service/impl/AuthenticationServiceTest.kt
+++ src/test/kotlin/com/invictoprojects/marketplace/service/impl/AuthenticationServiceTest.kt
@@ -1,14 +1,22 @@
 package com.invictoprojects.marketplace.service.impl
 
 import com.invictoprojects.marketplace.config.JwtProvider
+import com.invictoprojects.marketplace.dto.RegisterRequest
+import com.invictoprojects.marketplace.persistence.model.Role
+import com.invictoprojects.marketplace.persistence.model.User
 import com.invictoprojects.marketplace.service.RefreshTokenService
 import com.invictoprojects.marketplace.service.UserService
+import io.mockk.confirmVerified
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.crypto.password.PasswordEncoder
+import java.time.Instant
 
 @ExtendWith(MockKExtension::class)
 class AuthenticationServiceTest {
@@ -41,5 +49,19 @@ class AuthenticationServiceTest {
         )
     }
 
-    fun testSignUp() {}
+    @Test
+    fun testSignUp() {
+        val instant = Instant.now()
+        val user = User("user", "email@gmail.com", "passwordHash", instant, Role.USER, true, true)
+
+        every { passwordEncoder.encode("password") } returns "passwordHash"
+        every { userService.create("user", "email@gmail.com", "passwordHash") } returns user
+
+        val registerRequest = RegisterRequest("email@gmail.com", "user", "password")
+
+        authenticationService.signup(registerRequest)
+
+        verify { userService.create("user", "email@gmail.com", "passwordHash") }
+        confirmVerified(userService)
+    }
 }
