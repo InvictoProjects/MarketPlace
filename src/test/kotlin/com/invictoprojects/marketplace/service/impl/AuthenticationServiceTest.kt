b00908e34e5920f47c4e5f58b44e723d1c071fb4
Vlad <moyfolder@gmail.com>
2022-06-21 20:14:19 +0300

    Adding dependencies

--- src/test/kotlin/com/invictoprojects/marketplace/service/impl/AuthenticationServiceTest.kt
+++ src/test/kotlin/com/invictoprojects/marketplace/service/impl/AuthenticationServiceTest.kt
@@ -3,21 +3,43 @@ package com.invictoprojects.marketplace.service.impl
 import com.invictoprojects.marketplace.config.JwtProvider
 import com.invictoprojects.marketplace.service.RefreshTokenService
 import com.invictoprojects.marketplace.service.UserService
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.BeforeEach
-
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.crypto.password.PasswordEncoder
 
-internal class AuthenticationServiceTest {
-    private var userService: UserService? = null
-    private val passwordEncoder: PasswordEncoder? = null
-    private val authenticationManager: AuthenticationManager? = null
-    private val jwtProvider: JwtProvider? = null
-    private val refreshTokenService: RefreshTokenService? = null
+@ExtendWith(MockKExtension::class)
+class AuthenticationServiceTest {
+
+    @MockK
+    private lateinit var userService: UserService
+
+    @MockK
+    private lateinit var passwordEncoder: PasswordEncoder
+
+    @MockK
+    private lateinit var authenticationManager: AuthenticationManager
+
+    @MockK
+    private lateinit var jwtProvider: JwtProvider
+
+    @MockK
+    private lateinit var refreshTokenService: RefreshTokenService
+
+    private lateinit var authenticationService: AuthenticationServiceImpl
 
     @BeforeEach
     fun setUp() {
-
+        authenticationService = AuthenticationServiceImpl(
+            userService,
+            passwordEncoder,
+            authenticationManager,
+            jwtProvider,
+            refreshTokenService
+        )
     }
+
+    fun testSignUp() {}
 }
